e99ccf3c32ae17b9f5e10fca6412b459bbe53594
Jiahui Xu <jiahui0020@gmail.com>
2022-10-28 17:19:57 +0200

    benchmark

--- benchmarks/transformer.sh
+++ benchmarks/transformer.sh
@@ -1,2 +1,18 @@
 python transformer_demo.py --label_type next_log --feature_type sequentials --topk 10 --dataset HDFS --data_dir ../data/processed/HDFS/hdfs_0.0_tar
-python transformer_demo.py --label_type next_log --feature_type sequentials --topk 50 --dataset BGL --data_dir ../data/processed/BGL/bgl_0.0_tar
+
+# for lr in 0.001 0.0001;
+# do
+#     for ba in 1024 2048 4096;
+#     do
+#         for w in 1 10 100;
+#         do
+#             python transformer_demo.py \
+#                     --learning_rate ${lr} \
+#                     --batch_size ${ba} \
+#                     --window_size ${w} \
+#                     --label_type next_log --feature_type semantics --topk 10 --dataset HDFS \
+#                     --data_dir /Users/jiahui/deep-loglizer/data/processed/HDFS_Drain_result01/hdfs_1.0_tar  
+#             sleep 10
+#         done
+#     done
+# done
